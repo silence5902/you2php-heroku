3726ed2f90e1fc4710186b623bfb1c0d877b1184
silence5902 <45921032+silence5902@users.noreply.github.com>
2018-12-17 08:16:57 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,6 +1,6 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
+define('APIKEY', 'AIzaSyDUm1v6NWtE_7vjj5rwzLGm2E5TrBBQ5oM');//这里改成您的api key
 define('GJ_CODE', 'KR');//国家代码
 define('SITE_NAME', 'Youtube');//网站名字
 define('TITLENAME', 'Youtube');//网站名字
