bd6c3cd4de7b0529117f8e75fff950f86dbecb1c
silence5902 <silence5902@qq.com>
2018-12-19 21:15:54 +0800

    1
    1

---
+++ web/README.md
@@ -0,0 +1,12 @@
+# YOU2PHP-GL-SELECTOR
+让YOU2PHP浏览器可以选择缺省国家和地区
+
+1）安装YOU2PHP，链接：https://github.com/You2php/you2php
+
+2）下载本项目的三个文件config.php，header.php，select_default_gl.php
+
+3）编辑config.php，根据内容提示填写相应信息然后保存
+
+4）把这三个文件复制/覆盖到YOU2PHP目录
+
+![alt text](https://github.com/hahadaba/YOU2PHP-GL-SELECTOR/raw/master/you2php-gl-selector.jpg)
--- web/config.php
+++ web/config.php
@@ -1,9 +1,10 @@
 <?php
+$gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'HK';
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyDUm1v6NWtE_7vjj5rwzLGm2E5TrBBQ5oM');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('APIKEY', 'AIzaSyDUm1v6NWtE_7vjj5rwzLGm2E5TrBBQ5oM');
+define('GJ_CODE', $gl);
+define('SITE_NAME', 'bilibili');
+define('TITLENAME', 'bilibili');
+define('EN2DEKEY', '0090909i604wkkfkak');
+define('EMAIL', '你的邮箱');
 ?>
